7a6a0e718601ff8487297ee149e2cb9f6595c968
lilaspourpre <irina.nikishina@mail.ru>
2019-12-30 21:09:59 +0300

    update scoring program

--- scoring_program/utils.py
+++ scoring_program/utils.py
@@ -1,15 +1,16 @@
 from collections import defaultdict
 import codecs
 import os
+import json
 
 
-def read_dataset(data_path, sep='\t'):
+def read_dataset(data_path, read_fn=lambda x: x, sep='\t'):
     vocab = defaultdict(list)
     with codecs.open(data_path, 'r', encoding='utf-8') as f:
         for line in f:
             line_split = line.replace("\n", '').split(sep)
             word = line_split[0]
-            hypernyms = line_split[1]
+            hypernyms = read_fn(line_split[1])
             vocab[word].append(hypernyms)
     return vocab
 
@@ -27,6 +28,7 @@ def get_reference(parent):
     names = [os.path.join(parent, name) for name in os.listdir(parent) if (name.endswith('.tsv') or name.endswith('.csv'))]
     if len(names) == 0:
         raise RuntimeError('No .csv or .tsv files in reference')
-    if len(names) != 2:
-        raise RuntimeError('There should be exact two files in reference: {}'.format(' '.join(names)))
-    return read_dataset(names[0]), read_dataset(names[1])
+    if len(names) != 1:
+        raise RuntimeError('There should be exact one file in reference: {}'.format(' '.join(names)))
+    read_fn = lambda x: json.loads(x)
+    return read_dataset(names[0], read_fn)
