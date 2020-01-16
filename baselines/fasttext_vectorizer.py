3f8ffd1f553760ecd3756a155e0d1732f67eabe8
lilaspourpre <irina.nikishina@mail.ru>
2020-01-16 12:09:22 +0300

    delete abstract vectorizer class and improve fasttest vectorizer

--- baselines/fasttext_vectorizer.py
+++ baselines/fasttext_vectorizer.py
@@ -1,13 +1,10 @@
 import numpy as np
 from gensim.models.fasttext import load_facebook_model
 from string import punctuation
-from ruwordnet.ruwordnet_reader import RuWordnet
-from vectorizer import Vectorizer
 
 
-class FasttextVectorizer(Vectorizer):
+class FasttextVectorizer:
     def __init__(self, model_path):
-        super().__init__()
         self.model = load_facebook_model(model_path)
 
     # -------------------------------------------------------------
@@ -16,7 +13,7 @@ class FasttextVectorizer(Vectorizer):
 
     def vectorize_ruwordnet(self, synsets, output_path):
         rwn_vectors = self.__get_ruwordnet_vectors(synsets)
-        self.__save_as_w2v([i[0] for i in synsets], rwn_vectors, output_path)
+        self.save_as_w2v([i[0] for i in synsets], rwn_vectors, output_path)
 
     def __get_ruwordnet_vectors(self, synsets):
         vectors = np.zeros((len(synsets), self.model.vector_size))
@@ -34,7 +31,7 @@ class FasttextVectorizer(Vectorizer):
 
     def vectorize_data(self, data, output_path):
         data_vectors = self.__get_data_vectors(data)
-        self.__save_as_w2v(data, data_vectors, output_path)
+        self.save_as_w2v(data, data_vectors, output_path)
 
     def __get_data_vectors(self, data):
         vectors = np.zeros((len(data), self.model.vector_size))
@@ -42,14 +39,17 @@ class FasttextVectorizer(Vectorizer):
             vectors[i, :] = self.model[word]
         return vectors
 
+    def save_as_w2v(self, words: list, vectors: np.array, output_path):
+        assert len(words) == len(vectors)
+        with open(output_path, 'w', encoding='utf-8') as w:
+            w.write(f"{vectors.shape[0]} {vectors.shape[1]}\n")
+            for word, vector in zip(words, vectors):
+                vector_line = " ".join(map(str, vector))
+                w.write(f"{word} {vector_line}\n")
+
 
 if __name__ == '__main__':
     ft_vec = FasttextVectorizer("models/cc.ru.300.bin")
-    ruwordnet = RuWordnet(db_path="../dataset/ruwordnet.db", ruwordnet_path=None)
-
-    v_synsets = ruwordnet.get_all_synsets(endswith="V")
-    ft_vec.vectorize_ruwordnet(v_synsets, "models/vectors/ruwordnet_verbs_fasttext_single.txt")
-
-    n_synsets = ruwordnet.get_all_synsets(endswith="N")
-    ft_vec.vectorize_ruwordnet(n_synsets, "models/vectors/ruwordnet_nouns_fasttext_single.txt")
-
+    with open("../dataset/public/verbs_public_no_labels.tsv", 'r', encoding='utf-8') as f:
+        dataset = f.read().split("\n")[:-1]
+    ft_vec.vectorize_data(dataset, "models/vectors/verbs_public_fasttext.txt")
