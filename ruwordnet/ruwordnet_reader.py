17781cc1b726a1bed6f7cd9043062973892e011e
lilaspourpre <irina.nikishina@mail.ru>
2020-01-23 16:33:01 +0300

    add option to use db with lemma senses

--- ruwordnet/ruwordnet_reader.py
+++ ruwordnet/ruwordnet_reader.py
@@ -26,10 +26,16 @@ def parse_relations(file):
     return relations
 
 
-def parse_senses(file):
+def parse_senses_lemmas(file):
     soup = get_soup(file)
     return [(el.attrs['id'], element.attrs['id'], el.text) for element in soup.findAll('synset')
            for el in element.findAll('sense')]
+
+
+def parse_senses(file):
+    soup = get_soup(file)
+    return [(element.attrs['id'], element.attrs['synset_id'], element.attrs['name']) for element in
+            soup.findAll('sense')]
 
 
 def get_wordnet_files_from_path(path):
@@ -48,18 +54,24 @@ def get_wordnet_files_from_path(path):
 
 
 class RuWordnet(DatabaseRuWordnet):
-    def __init__(self, db_path, ruwordnet_path):
+    def __init__(self, db_path, ruwordnet_path, with_lemmas=False):
         super(RuWordnet, self).__init__(db_path)
         self.__initialize_db(ruwordnet_path)
+        self.with_lemmas = with_lemmas
 
     def __initialize_db(self, path):
         if self.is_empty():
             print("Inserting data to database")
-            synset_files, relation_files, _ = get_wordnet_files_from_path(path)
+            synset_files, relation_files, senses_files = get_wordnet_files_from_path(path)
+
             synsets = [synset for file in synset_files for synset in parse_synsets(file)]
             relations = [relation for file in relation_files for relation in parse_relations(file)]
-            senses = [sense for file in synset_files for sense in parse_senses(file)]
+
+            if self.with_lemmas:
+                senses = [sense for file in synset_files for sense in parse_senses_lemmas(file)]
+            else:
+                senses = [sense for file in senses_files for sense in parse_senses(file)]
+
             self.insert_synsets(synsets)
             self.insert_relations(relations)
             self.insert_senses(senses)
-
