a68e6e837f0318847812b736829d341674453369
lilaspourpre <irina.nikishina@mail.ru>
2020-01-23 16:29:19 +0300

    update prediction script

--- baselines/main.py
+++ baselines/main.py
@@ -1,70 +1,15 @@
 import sys
 import json
 import codecs
-from abc import abstractmethod
 
-from ruwordnet.ruwordnet_reader import RuWordnet
-from gensim.models import KeyedVectors
+from predict_models import BaselineModel, SecondOrderModel
 
 
-class Model:
-    @abstractmethod
-    def predict_hypernyms(self, neologisms, topn=10):
-        pass
-
-    @abstractmethod
-    def __compute_hypernyms(self, neologisms, topn=10):
-        pass
-
-
-class BaselineModel(Model):
-    def __init__(self, params):
-        self.ruwordnet = RuWordnet(db_path=params["db_path"], ruwordnet_path=params["ruwordnet_path"])
-        self.w2v_ruwordnet = KeyedVectors.load_word2vec_format(params['ruwordnet_vectors_path'], binary=False)
-        self.w2v_data = KeyedVectors.load_word2vec_format(params['data_vectors_path'], binary=False)
-
-    def predict_hypernyms(self, neologisms, topn=10) -> dict:
-        return {neologism: self.__compute_hypernyms(neologism, topn) for neologism in neologisms}
-
-    def __compute_hypernyms(self, neologism, topn=10) -> list:
-        return [i[0] for i in self.w2v_ruwordnet.similar_by_vector(self.w2v_data[neologism], topn)]
-
-
-class SecondOrderModel(Model):
-    def __init__(self, params):
-        self.ruwordnet = RuWordnet(db_path=params["db_path"], ruwordnet_path=params["ruwordnet_path"])
-        self.w2v_ruwordnet = KeyedVectors.load_word2vec_format(params['ruwordnet_vectors_path'], binary=False)
-        self.w2v_data = KeyedVectors.load_word2vec_format(params['data_vectors_path'], binary=False)
-
-    def predict_hypernyms(self, neologisms, topn=10) -> dict:
-        return {neologism: self.__compute_hypernyms(neologism, topn) for neologism in neologisms}
-
-    def __compute_hypernyms(self, neologism, topn=10) -> list:
-        hypernyms = []
-        associates = [i[0] for i in self.w2v_ruwordnet.similar_by_vector(self.w2v_data[neologism], topn)]
-        for associate in associates:
-            hypernyms.extend(self.ruwordnet.get_hypernyms_by_id(associate))
-        return hypernyms[:10]
-
-
-class OneSecondModel(Model):
-    def __init__(self, params):
-        self.ruwordnet = RuWordnet(db_path=params["db_path"], ruwordnet_path=params["ruwordnet_path"])
-        self.w2v_ruwordnet = KeyedVectors.load_word2vec_format(params['ruwordnet_vectors_path'], binary=False)
-        self.w2v_data = KeyedVectors.load_word2vec_format(params['data_vectors_path'], binary=False)
-
-    def predict_hypernyms(self, neologisms, topn=10) -> dict:
-        return {neologism: self.__compute_hypernyms(neologism, topn) for neologism in neologisms}
-
-    def __compute_hypernyms(self, neologism, topn=10) -> list:
-        hypernyms = []
-        associates = [i[0] for i in self.w2v_ruwordnet.similar_by_vector(self.w2v_data[neologism], topn)]
-        for associate in associates:
-            try:
-                hypernyms.append(self.ruwordnet.get_hypernyms_by_id(associate)[0])
-            except Exception as e:
-                hypernyms.append(associate)
-        return hypernyms
+def save_to_file(words_with_hypernyms, output_path, ruwordnet):
+    with codecs.open(output_path, 'w', encoding='utf-8') as f:
+        for word, hypernyms in words_with_hypernyms.items():
+            for hypernym in hypernyms:
+                f.write(f"{word}\t{hypernym}\t{ruwordnet.get_name_by_id(hypernym)}\n")
 
 
 def load_config():
@@ -75,18 +20,12 @@ def load_config():
     return params
 
 
-def save_to_file(words_with_hypernyms, output_path, ruwordnet):
-    with codecs.open(output_path, 'w', encoding='utf-8') as f:
-        for word, hypernyms in words_with_hypernyms.items():
-            for hypernym in hypernyms:
-                f.write(f"{word}\t{hypernym}\t{ruwordnet.get_name_by_id(hypernym)}\n")
-
-
 def main():
+    models = {"baseline": BaselineModel, "second_order": SecondOrderModel}
     params = load_config()
     with open(params['test_path'], 'r', encoding='utf-8') as f:
         test_data = f.read().split("\n")[:-1]
-    baseline = OneSecondModel(params)
+    baseline = models[params["model"]](params)
     print("Model loaded")
     results = baseline.predict_hypernyms(list(test_data))
     save_to_file(results, params['output_path'], baseline.ruwordnet)
