eba61742f95811b9c7cb9b0c505cef33e41a1d24
lilaspourpre <irina.nikishina@mail.ru>
2020-03-30 18:57:29 +0300

    add fasttext vectorizer for English

---
+++ baselines/fasttext_vectorize_en.py
@@ -0,0 +1,49 @@
+from nltk.corpus import WordNetCorpusReader
+from collections import defaultdict
+from vectorizers.fasttext_vectorizer import FasttextVectorizer
+import os
+
+
+def compute_synsets_from_wordnets(old_wordnet, new_wordnet, pos):
+    synsets_old = set(old_wordnet.all_synsets(pos))
+    synsets_new = set(new_wordnet.all_synsets(pos))
+    reference = synsets_new.intersection(synsets_old)
+    return extract_senses(reference)
+
+
+def extract_senses(synsets) -> dict:
+    result = defaultdict(list)
+    for synset in synsets:
+        for lemma in synset.lemmas():
+            result[synset.name()].append(lemma.name().replace("_", " "))
+    return result
+
+
+def process_data(vectorizer, input_file, output_file):
+    with open(input_file, 'r', encoding='utf-8') as f:
+        dataset = f.read().split("\n")[:-1]
+    vectorizer.vectorize_multiword_data(dataset, output_file, to_upper=False)
+
+
+def main():
+    ft = FasttextVectorizer("models/cc.en.300.bin")
+    wn2 = WordNetCorpusReader('D:\\dialogue2020\\semeval-2016-task-14\\WN2.0', None)
+    wn3 = WordNetCorpusReader('D:\\dialogue2020\\semeval-2016-task-14\\WN3.0', None)
+    input_path = "D:/dialogue2020/semeval-2016-task-14/reader/"
+    vector_path = "models/vectors/fasttext/en/"
+
+    # vectorize wordnet
+    noun_synsets = compute_synsets_from_wordnets(wn2, wn3, 'n')
+    verb_synsets = compute_synsets_from_wordnets(wn2, wn3, 'v')
+    ft.vectorize_groups(noun_synsets, os.path.join(vector_path, "nouns_wordnet_fasttext.txt"), to_upper=False)
+    ft.vectorize_groups(verb_synsets, os.path.join(vector_path, "verbs_wordnet_fasttext.txt"), to_upper=False)
+
+    # vectorize words
+    process_data(ft, os.path.join(input_path, "no_labels_nouns_en_new.tsv"),
+                 os.path.join(vector_path, "nouns_fasttext.txt"))
+    process_data(ft, os.path.join(input_path, "no_labels_verbs_en_new.tsv"),
+                 os.path.join(vector_path, "verbs_fasttext.txt"))
+
+
+if __name__ == '__main__':
+    main()
