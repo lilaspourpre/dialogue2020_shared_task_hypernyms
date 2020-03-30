08a2d7a873663748589038585ebd9ddcfa95c13e
lilaspourpre <irina.nikishina@mail.ru>
2020-03-30 18:56:26 +0300

    revise fasttext_vectorizer for ru

---
+++ baselines/fasttext_vectorize_ru.py
@@ -0,0 +1,40 @@
+import os
+from collections import defaultdict
+
+from ruwordnet.ruwordnet_reader import RuWordnet
+from vectorizers.fasttext_vectorizer import FasttextVectorizer
+
+
+def process_data(vectorizer, input_file, output_file):
+    with open(input_file, 'r', encoding='utf-8') as f:
+        dataset = f.read().lower().split("\n")[:-1]
+    vectorizer.vectorize_words(dataset, output_file)
+
+
+if __name__ == '__main__':
+    ft = FasttextVectorizer("models/cc.ru.300.bin")
+    ruwordnet = RuWordnet(db_path="../dataset/ruwordnet.db", ruwordnet_path=None)
+    vector_path = "models/vectors/fasttext/ru/"
+
+    # ----------------------
+    # vectorize synsets
+    # ----------------------
+    noun_synsets = defaultdict(list)
+    verb_synsets = defaultdict(list)
+
+    for sense_id, synset_id, text in ruwordnet.get_all_senses():
+        if synset_id.endswith("N"):
+            noun_synsets[synset_id].append(text.lower())
+        elif synset_id.endswith("V"):
+            verb_synsets[synset_id].append(text.lower())
+
+    ft.vectorize_groups(noun_synsets, os.path.join(vector_path, "ruwordnet_nouns_new.txt"))
+    ft.vectorize_groups(verb_synsets, os.path.join(vector_path, "ruwordnet_verbs_new.txt"))
+
+    # ----------------------
+    # vectorize data
+    # ----------------------
+    process_data(ft, "../dataset/public/verbs_public_no_labels.tsv", os.path.join(vector_path, "verbs_public_new.txt"))
+    process_data(ft, "../dataset/public/nouns_public_no_labels.tsv", os.path.join(vector_path, "nouns_public_new.txt"))
+    process_data(ft, "../dataset/private/verbs_private_no_labels.tsv", os.path.join(vector_path, "verbs_private_new.txt"))
+    process_data(ft, "../dataset/private/nouns_private_no_labels.tsv", os.path.join(vector_path, "nouns_private_new.txt"))
