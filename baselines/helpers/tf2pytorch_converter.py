85d7cd0099fc1bd3f5516fae9cb1d08309d5942f
lilaspourpre <irina.nikishina@mail.ru>
2020-04-05 18:39:40 +0300

    improve tf2pytorch_converter.py

--- baselines/helpers/tf2pytorch_converter.py
+++ baselines/helpers/tf2pytorch_converter.py
@@ -1,11 +1,15 @@
-import torch
+import sys
 from pytorch_pretrained_bert import convert_tf_checkpoint_to_pytorch
-from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
+from pytorch_pretrained_bert import BertTokenizer
 
-BERT_MODEL_PATH = '../models/rubert_cased_deeppavlov/'
-tokenizer = BertTokenizer.from_pretrained(BERT_MODEL_PATH, cache_dir=None,do_lower_case=False)
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        raise Exception("Please specify BERT path")
 
-convert_tf_checkpoint_to_pytorch.convert_tf_checkpoint_to_pytorch(
-    BERT_MODEL_PATH + 'bert_model.ckpt',
-    BERT_MODEL_PATH + 'bert_config.json',
-    '../models/rubert_cased_torch/pytorch_model.bin')
+    BERT_MODEL_PATH = sys.argv[1]  # ../models/pretrained_berts/enbert_cased_torch/
+    tokenizer = BertTokenizer.from_pretrained(BERT_MODEL_PATH, cache_dir=None, do_lower_case=False)
+
+    convert_tf_checkpoint_to_pytorch.convert_tf_checkpoint_to_pytorch(
+        BERT_MODEL_PATH + 'bert_model.ckpt',
+        BERT_MODEL_PATH + 'bert_config.json',
+        BERT_MODEL_PATH + 'pytorch_model.bin')
