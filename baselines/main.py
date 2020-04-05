c1d98366b4c895523fbb87ab743ca4291b292806
lilaspourpre <irina.nikishina@mail.ru>
2020-04-05 18:29:49 +0300

    improve predict_models.py (split into three classes, adapt for both languages)

--- baselines/main.py
+++ baselines/main.py
@@ -2,14 +2,10 @@ import sys
 import json
 import codecs
 
-from predict_models import BaselineModel, SecondOrderModel
+from nltk.corpus import WordNetCorpusReader
 
-
-def save_to_file(words_with_hypernyms, output_path, ruwordnet):
-    with codecs.open(output_path, 'w', encoding='utf-8') as f:
-        for word, hypernyms in words_with_hypernyms.items():
-            for hypernym in hypernyms:
-                f.write(f"{word}\t{hypernym}\t{ruwordnet.get_name_by_id(hypernym)}\n")
+from ruwordnet.ruwordnet_reader import RuWordnet
+from predict_models import BaselineModel, HCHModel, RankedModel
 
 
 def load_config():
@@ -20,15 +16,37 @@ def load_config():
     return params
 
 
+def generate_hypernym_fn(params, model):
+    # for RuWordNet
+    if params['language'] == 'en':
+        wn = WordNetCorpusReader(params["ruwordnet_path"], None)
+        return lambda x: [hypernym.name() for hypernym in wn.synset(x).hypernyms()
+                          if hypernym.name() in model.w2v_synsets.vocab]
+    # for English WordNet
+    elif params['language'] == 'ru':
+        ruwordnet = RuWordnet(db_path=params["db_path"], ruwordnet_path=params["ruwordnet_path"])
+        return lambda x: ruwordnet.get_hypernyms_by_id(x)
+    else:
+        raise Exception("language is not supported")
+
+
+def save_to_file(words_with_hypernyms, output_path):
+    with codecs.open(output_path, 'w', encoding='utf-8') as f:
+        for word, hypernyms in words_with_hypernyms.items():
+            for hypernym in hypernyms:
+                f.write(f"{word}\t{hypernym}\n")
+
+
 def main():
-    models = {"baseline": BaselineModel, "second_order": SecondOrderModel}
+    models = {"baseline": BaselineModel, "hch": HCHModel, "ranked": RankedModel}
     params = load_config()
     with open(params['test_path'], 'r', encoding='utf-8') as f:
         test_data = f.read().split("\n")[:-1]
-    baseline = models[params["model"]](params)
+    model = models[params["model"]](params)
     print("Model loaded")
-    results = baseline.predict_hypernyms(list(test_data))
-    save_to_file(results, params['output_path'], baseline.ruwordnet)
+
+    results = model.predict_hypernyms(list(test_data), generate_hypernym_fn(params, model))
+    save_to_file(results, params['output_path'])
 
 
 if __name__ == '__main__':
--- baselines/predict_models.py
+++ baselines/predict_models.py
@@ -1,45 +1,79 @@
-from abc import abstractmethod
+from abc import abstractmethod, ABC
+from collections import Counter
+from gensim.models import KeyedVectors
+from scipy import spatial
+from operator import itemgetter
 
 from ruwordnet.ruwordnet_reader import RuWordnet
-from gensim.models import KeyedVectors
 
 
-class Model:
+class Model(ABC):
     def __init__(self, params):
-        self.ruwordnet = RuWordnet(db_path=params["db_path"], ruwordnet_path=params["ruwordnet_path"])
-        self.w2v_ruwordnet = KeyedVectors.load_word2vec_format(params['ruwordnet_vectors_path'], binary=False)
+        self.w2v_synsets = KeyedVectors.load_word2vec_format(params['synsets_vectors_path'], binary=False)
         self.w2v_data = KeyedVectors.load_word2vec_format(params['data_vectors_path'], binary=False)
 
-    @abstractmethod
-    def predict_hypernyms(self, neologisms, topn=10):
-        pass
+    def predict_hypernyms(self, neologisms, get_hypernym_fn, topn=10):
+        return {neologism: self.compute_candidates(neologism, get_hypernym_fn, topn) for neologism in neologisms}
 
     @abstractmethod
-    def __compute_hypernyms(self, neologisms, topn=10):
+    def compute_candidates(self, neologisms, get_hypernym_fn, topn=10):
         pass
 
+
+# ---------------------------------------------------------------------------------------------
+# Baseline Model
+# ---------------------------------------------------------------------------------------------
 
 class BaselineModel(Model):
     def __init__(self, params):
         super().__init__(params)
 
-    def predict_hypernyms(self, neologisms, topn=10) -> dict:
-        return {neologism: self.__compute_hypernyms(neologism, topn) for neologism in neologisms}
+    def compute_candidates(self, neologism, get_hypernym_fn, topn=10) -> list:
+        return list(map(itemgetter(0), self.generate_associates(neologism, topn)))
 
-    def __compute_hypernyms(self, neologism, topn=10) -> list:
-        return [i[0] for i in self.w2v_ruwordnet.similar_by_vector(self.w2v_data[neologism], topn)]
+    def generate_associates(self, neologism, topn=10) -> list:
+        return self.w2v_synsets.similar_by_vector(self.w2v_data[neologism], topn)
 
 
-class SecondOrderModel(Model):
+# ---------------------------------------------------------------------------------------------
+# Hypernym of Co-Hypernyms Model
+# ---------------------------------------------------------------------------------------------
+
+class HCHModel(BaselineModel):
     def __init__(self, params):
         super().__init__(params)
 
-    def predict_hypernyms(self, neologisms, topn=10) -> dict:
-        return {neologism: self.__compute_hypernyms(neologism, topn) for neologism in neologisms}
+    def compute_candidates(self, neologism, get_hypernym_fn, topn=10):
+        return self.compute_hchs(neologism, get_hypernym_fn, topn=10)[:topn]
 
-    def __compute_hypernyms(self, neologism, topn=10) -> list:
-        hypernyms = []
-        associates = [i[0] for i in self.w2v_ruwordnet.similar_by_vector(self.w2v_data[neologism], topn)]
-        for associate in associates:
-            hypernyms.extend(self.ruwordnet.get_hypernyms_by_id(associate))
-        return hypernyms[:10]
+    def compute_hchs(self, neologism, compute_hypernyms, topn=10) -> list:
+        associates = map(itemgetter(0), self.generate_associates(neologism, topn))
+        hchs = [hypernym for associate in associates for hypernym in compute_hypernyms(associate)]
+        return hchs
+
+
+# ---------------------------------------------------------------------------------------------
+# Ranked Model
+# ---------------------------------------------------------------------------------------------
+
+class RankedModel(HCHModel):
+    def __init__(self, params):
+        super().__init__(params)
+        self.ruwordnet = RuWordnet(db_path=params["db_path"], ruwordnet_path=params["ruwordnet_path"])
+
+    def compute_candidates(self, neologism, get_hypernym_fn, topn=10):
+        hypernyms = self.compute_hchs(neologism, get_hypernym_fn, topn)
+        second_order_hypernyms = [s_o for hypernym in hypernyms for s_o in get_hypernym_fn(hypernym)]
+
+        all_hypernyms = Counter(hypernyms + second_order_hypernyms)
+        sorted_hypernyms = reversed(sorted(all_hypernyms.items(), key=lambda x: self.__get_score(neologism, *x)))
+
+        return [i[0] for i in sorted_hypernyms][:topn]
+
+    def __get_score(self, neologism, candidate, count):
+        return count * self.__get_similarity(neologism, candidate)
+
+    def __get_similarity(self, neologism, candidate):
+        v1 = self.w2v_data[neologism]
+        v2 = self.w2v_synsets[candidate]
+        return 1 - spatial.distance.cosine(v1, v2)
